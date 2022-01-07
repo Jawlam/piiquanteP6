cf7f5558d570fb394e98f594e56b45c90267467b
jaouad <lamarti.jaouad@icloud.com>
2022-01-07 11:05:16 +0100

    mise en place des controllers, models, routes et middleware auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+
 mongoose
   .connect(
     "",
@@ -12,22 +16,22 @@ mongoose
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
---
+++ backend/models/sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const sauceSchema = mongoose.Schema({
+  userId: { type: mongoose.ObjectId, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: [mongoose.ObjectId], required: true },
+  usersDisliked: { type: [mongoose.ObjectId], required: true },
+});
+
+module.exports = mongoose.model("Sauce", sauceSchema);
