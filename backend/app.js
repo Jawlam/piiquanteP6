f95796c3c896ac72ca33813c9afd28a9e55f61cf
jaouad <lamarti.jaouad@icloud.com>
2022-01-07 14:02:01 +0100

    configurations + import multer

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const express = require('express');
+const mongoose = require('mongoose');
+//const bodyParser = require('body-parser');
+const path = require('path'); // Donne accès au système de fichier
+
+
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
 
@@ -29,7 +35,10 @@ app.use((req, res, next) => {
 });
 
 
-app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use("/api/sauces", sauceRoutes);
 app.use("/api/auth", userRoutes);
